b8be6ff2508c2593d79405d43eebc701b576d2f7
Christoph Dorn <christoph@christophdorn.com>
2014-12-17 23:10:40 -0800

    kill process if cpu or memory limit reached

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
 
+const PATH = require("path");
+const FS = require("fs");
+const EXEC = require("child_process").exec;
+
+const LIST = require("./widgets/service/list.json.service.js");
+
+
+var pioConfig = JSON.parse(FS.readFileSync(PATH.join(__dirname, "../.pio.json"), "utf8"));
+
+
 require("io.pinf.server.www").for(module, __dirname, function(app, config, HELPERS) {
 
 /*
@@ -21,5 +31,136 @@ require("io.pinf.server.www").for(module, __dirname, function(app, config, HELPE
 	}
 	recordRequest();
 */
+
+	function killProcess (pid, callback) {
+		console.log("Killing process:", pid);
+		delete watchingAlerts[process.info.PID];
+		return EXEC("kill -9 " + pid, function (err, stdout, stderr) {
+			if (err) return callback(err);
+			console.log("Process killed:", pid);
+			return callback(null);
+		});
+	}
+
+	var watchingAlerts = {};
+
+	function triggerAlert (serviceId, process, alertInfo) {
+
+		console.log("Service", serviceId, "and process", process, "has reached limit", alertInfo.value, "for field", alertInfo.field);
+
+		if (!watchingAlerts[process.info.PID + "-" + alertInfo._id]) {
+			console.log("Keeping an eye on process", process.info.PID, "due to", process.info[alertInfo.field]);
+			watchingAlerts[process.info.PID + "-" + alertInfo._id] = process;
+		} else
+		if (
+			parseInt(process.info[alertInfo.field]) >
+			parseInt(watchingAlerts[process.info.PID + "-" + alertInfo._id].info[alertInfo.field])
+		) {
+			killProcess(process.info.PID, function (err) {
+				if (err) {
+					console.error("Error killing process", process.info.PID, err.stack);
+				}
+			});
+		}
+	}
+
+	function checkIfClearAlert (serviceId, process, alertInfo) {
+		if (!watchingAlerts[process.info.PID + "-" + alertInfo._id]) {
+			return;
+		}
+		console.log("Usage went down from", watchingAlerts[process.info.PID + "-" + alertInfo._id].info[alertInfo.field], "to", process.info[alertInfo.field], ". No longer keeping eye on process", process.info.PID);
+		delete watchingAlerts[process.info.PID + "-" + alertInfo._id];
+	}
+
+	function setupAlerts (callback) {
+		if (
+			!config.config ||
+			!config.config.alerts
+		) {
+			return callback(null);
+		}
+
+		var alerts = {};
+
+		var defaultConfig = config.config.alerts['*'] || {};
+
+		// TODO: Merge in `config.plugin` service specific config.
+
+		pioConfig.config["pio.services"].order.forEach(function (serviceId) {
+			var mergedConfig = HELPERS.API.DEEPMERGE(defaultConfig, config.config.alerts[serviceId] || {});
+
+			console.log("Setup alert for service", serviceId, mergedConfig);
+
+			alerts[serviceId] = mergedConfig;
+		});
+
+		function checkAlerts (callback) {
+
+			console.log("Check alerts", (new Date()).toString());
+
+			return LIST.getProcesses(function (err, proceses) {
+				if (err) return callback(err);
+
+				function checkProcess (process) {
+
+					var pid = process.info.PID;
+
+					var triggered = false;
+					if (alerts[serviceId]) {
+						for (var alertId in alerts[serviceId]) {
+							var alertInfo = alerts[serviceId][alertId];
+							alertInfo._id = alertId;
+							if (alertInfo.type === "limit") {
+								if (parseInt(process.info[alertInfo.field]) > parseInt(alertInfo.value)) {
+									triggered = true;
+									triggerAlert(serviceId, process, alertInfo);
+								} else {
+									checkIfClearAlert(serviceId, process, alertInfo);
+								}
+							}
+
+						}
+					}
+
+					if (triggered) {
+						// We don't need to process children as they should be killed automatically
+						// because the parent process is gone.
+						// TODO: Watch and kill all children?
+						return;
+					}
+
+					if (process.children) {
+						process.children.forEach(function (pid) {
+							checkProcess(proceses.byPid[pid]);
+						});
+					}
+				}
+
+				for (var serviceId in proceses.byServiceId) {
+					checkProcess(proceses.byPid[proceses.byServiceId[serviceId]]);
+				}
+
+				return callback(null);
+			});
+		}
+
+		function checkAlertsInterval () {
+			checkAlerts(function (err) {
+				if (err) {
+					console.error("Error running alert checks", err.stack);
+				}
+			});
+		}
+		setInterval(checkAlertsInterval, 60 * 1000);
+		checkAlertsInterval();
+
+		return callback(null);
+	}
+
+	setupAlerts(function (err) {
+		if (err) {
+			console.error("Error setting up alerts", err.stack);
+		}
+	});
 
 });
--- widgets/service/list.json.service.js
+++ widgets/service/list.json.service.js
@@ -8,125 +8,127 @@ const SPAWN = require("child_process").spawn;
 var pioConfig = JSON.parse(FS.readFileSync(PATH.join(__dirname, "../../../.pio.json"), "utf8"));
 
 
-exports.app = function (req, res, next) {
+exports.getProcesses = function (callback) {
 
-	function getProcesses (callback) {
+	var processes = {
+		byPid: {},
+		byServiceId: {}
+	};
+	var columns;
 
-		var processes = {
-			byPid: {},
-			byServiceId: {}
-		};
-		var columns;
-
-		function makeRow (columns, fields) {
-			var row = {};
-			fields.forEach(function (field, index) {
-				if (columns[index]) {
-					row[columns[index]] = field;
-				} else {
-					row[columns[columns.length - 1]] += " " + field;
-				}
-			});
-			return row;
-		}
-
-
-		var proc = SPAWN("bash");
-		proc.stderr.on('data', function (data) {
-		  console.log('stderr: ' + data);
-		});
-		var buffer = [];
-		proc.stdout.on('data', function (data) {
-			buffer.push(data.toString());
-		});
-		proc.on('close', function (code) {
-			if (code !== 0) {
-				return callback(new Error("Process exit status != 0"));
+	function makeRow (columns, fields) {
+		var row = {};
+		fields.forEach(function (field, index) {
+			if (columns[index]) {
+				row[columns[index]] = field;
+			} else {
+				row[columns[columns.length - 1]] += " " + field;
 			}
-			columns = null;
-			buffer.join("").split("\n").forEach(function (line) {
-				if (!line) return;
-				var fields = line.replace(/[\t\s]+/g, " ").replace(/(^\s|\s$)/g, "").split(/\s/);
-
-				if (fields[0] === "PPID" || fields[0] === "USER") {
-					columns = fields;
-				} else {
-					// @see http://www.cs.miami.edu/~geoff/Courses/CSC521-04F/Content/UNIXProgramming/UNIXProcesses.shtml
-					// @see http://chinkisingh.com/2012/06/10/session-foreground-processes-background-processes-and-their-interaction-with-controlling-terminal/					
-					var process = makeRow(columns, fields);
-					// process.PID - Process ID
-					// process.PPID - Parent process ID
-					// process.PGID - Parent group ID
-					// process.SID - Session leader ID
-					// process.TPGID - Terminal process group ID
-					// process.TTY - (TeleTYpewriter) The terminal that executed a particular command ; @see http://stackoverflow.com/a/7113800/330439
-					// process.STAT - Process state ; @see http://unix.stackexchange.com/a/18477/92833
-					//	 states:
-					//		D Uninterruptible sleep (usually IO)
-					//		R Running or runnable (on run queue)
-					//		S Interruptible sleep (waiting for an event to complete)
-					//		T Stopped, either by a job control signal or because it is being traced.
-					//		W paging (not valid since the 2.6.xx kernel)
-					//		X dead (should never be seen)
-					//		Z Defunct ("zombie") process, terminated but not reaped by its parent.
-					//   flags:
-					//		< high-priority (not nice to other users)
-					//		N low-priority (nice to other users)
-					//		L has pages locked into memory (for real-time and custom IO)
-					//		s is a session leader
-					//		l is multi-threaded (using CLONE_THREAD, like NPTL pthreads do)
-					//		+ is in the foreground process group
-					// process.UID - User ID ; @see http://stackoverflow.com/a/205146/330439
-					// process.START - Indication of how long the process has been up
-					// process.TIME - Accumulated CPU utilization time ; @see http://www.theunixschool.com/2012/09/ps-command-what-does-time-indicate.html
-					// process.USER - Username of PID
-					// process.COMMAND - The command being executed
-					// process.%CPU - % of current total CPU utilization
-					// process.%MEM - % of current total MEM utilization
-					// process.VSZ - (Virtual Memory Size) Accessible memory including swap and shared lib ; @see http://stackoverflow.com/a/21049737/330439
-					// process.RSS - (Resident Set Size) Allocated ram ; @see http://stackoverflow.com/a/21049737/330439
-
-					if (!processes.byPid[process.PID]) {
-						processes.byPid[process.PID] = {};
-					}
-					if (!processes.byPid[process.PID].info) {
-						processes.byPid[process.PID].info = {};
-					}
-					for (var name in process) {
-						if (typeof processes.byPid[process.PID].info[name] === "undefined") {
-							processes.byPid[process.PID].info[name] = process[name];
-						}
-					}
-
-					if (process.COMMAND) {
-						var m = process.COMMAND.match(/\s\/opt\/run\/([^\/]+).pid.+UPSTART_JOB/);
-						if (m) {
-							processes.byServiceId[m[1]] = process.PID;
-						}
-					}
-
-					if (process.PPID) {
-						if (!processes.byPid[process.PPID]) {
-							processes.byPid[process.PPID] = {};
-						}
-						if (!processes.byPid[process.PPID].children) {
-							processes.byPid[process.PPID].children = [];
-						}
-						if (processes.byPid[process.PPID].children.indexOf(process.PID) === -1) {
-							processes.byPid[process.PPID].children.push(process.PID);
-						}
-					}
-
-				}
-			});
-
-			return callback(null, processes)
 		});
-		proc.stdin.write("ps axwwejH ; ps axuww");
-		return proc.stdin.end();
+		return row;
 	}
 
-	return getProcesses(function (err, proceses) {
+
+	var proc = SPAWN("bash");
+	proc.stderr.on('data', function (data) {
+	  console.log('stderr: ' + data);
+	});
+	var buffer = [];
+	proc.stdout.on('data', function (data) {
+		buffer.push(data.toString());
+	});
+	proc.on('close', function (code) {
+		if (code !== 0) {
+			return callback(new Error("Process exit status != 0"));
+		}
+		columns = null;
+		buffer.join("").split("\n").forEach(function (line) {
+			if (!line) return;
+			var fields = line.replace(/[\t\s]+/g, " ").replace(/(^\s|\s$)/g, "").split(/\s/);
+
+			if (fields[0] === "PPID" || fields[0] === "USER") {
+				columns = fields;
+			} else {
+				// @see http://www.cs.miami.edu/~geoff/Courses/CSC521-04F/Content/UNIXProgramming/UNIXProcesses.shtml
+				// @see http://chinkisingh.com/2012/06/10/session-foreground-processes-background-processes-and-their-interaction-with-controlling-terminal/					
+				var process = makeRow(columns, fields);
+				// process.PID - Process ID
+				// process.PPID - Parent process ID
+				// process.PGID - Parent group ID
+				// process.SID - Session leader ID
+				// process.TPGID - Terminal process group ID
+				// process.TTY - (TeleTYpewriter) The terminal that executed a particular command ; @see http://stackoverflow.com/a/7113800/330439
+				// process.STAT - Process state ; @see http://unix.stackexchange.com/a/18477/92833
+				//	 states:
+				//		D Uninterruptible sleep (usually IO)
+				//		R Running or runnable (on run queue)
+				//		S Interruptible sleep (waiting for an event to complete)
+				//		T Stopped, either by a job control signal or because it is being traced.
+				//		W paging (not valid since the 2.6.xx kernel)
+				//		X dead (should never be seen)
+				//		Z Defunct ("zombie") process, terminated but not reaped by its parent.
+				//   flags:
+				//		< high-priority (not nice to other users)
+				//		N low-priority (nice to other users)
+				//		L has pages locked into memory (for real-time and custom IO)
+				//		s is a session leader
+				//		l is multi-threaded (using CLONE_THREAD, like NPTL pthreads do)
+				//		+ is in the foreground process group
+				// process.UID - User ID ; @see http://stackoverflow.com/a/205146/330439
+				// process.START - Indication of how long the process has been up
+				// process.TIME - Accumulated CPU utilization time ; @see http://www.theunixschool.com/2012/09/ps-command-what-does-time-indicate.html
+				// process.USER - Username of PID
+				// process.COMMAND - The command being executed
+				// process.%CPU - % of current total CPU utilization
+				// process.%MEM - % of current total MEM utilization
+				// process.VSZ - (Virtual Memory Size) Accessible memory including swap and shared lib ; @see http://stackoverflow.com/a/21049737/330439
+				// process.RSS - (Resident Set Size) Allocated ram ; @see http://stackoverflow.com/a/21049737/330439
+
+				if (!processes.byPid[process.PID]) {
+					processes.byPid[process.PID] = {};
+				}
+				if (!processes.byPid[process.PID].info) {
+					processes.byPid[process.PID].info = {};
+				}
+				for (var name in process) {
+					if (typeof processes.byPid[process.PID].info[name] === "undefined") {
+						processes.byPid[process.PID].info[name] = process[name];
+					}
+				}
+
+				if (process.COMMAND) {
+					var m = process.COMMAND.match(/\s\/opt\/run\/([^\/]+).pid.+UPSTART_JOB/);
+					if (m) {
+						processes.byServiceId[m[1]] = process.PID;
+					}
+				}
+
+				if (process.PPID) {
+					if (!processes.byPid[process.PPID]) {
+						processes.byPid[process.PPID] = {};
+					}
+					if (!processes.byPid[process.PPID].children) {
+						processes.byPid[process.PPID].children = [];
+					}
+					if (processes.byPid[process.PPID].children.indexOf(process.PID) === -1) {
+						processes.byPid[process.PPID].children.push(process.PID);
+					}
+				}
+
+			}
+		});
+
+		return callback(null, processes)
+	});
+	proc.stdin.write("ps axwwejH ; ps axuww");
+	return proc.stdin.end();
+}
+
+
+
+exports.app = function (req, res, next) {
+
+	return exports.getProcesses(function (err, proceses) {
 		if (err) return next(err);
 
 		var data = {
